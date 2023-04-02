709fddec84eb12a5783a5e4e012fc64d2b7ff368
Claudia Moosbrugger <claudia.moosbrugger@live.at>
2023-04-02 15:17:21 +0200

    Part B Implement Header, Main, Footer and implement Information

--- towachallenge/src/App.vue
+++ towachallenge/src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <img class="logoTop" alt="catnyou logo" src="@/assets/catnyou.png">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/random">Random</router-link>
+    </nav>
+  </header>
+  <main>
+        <router-view/>
+  </main>
+  <footer>
+        <div class="container">
+          <div class="col"><img class="logoBottom" alt="catnyou logo" src="@/assets/catnyou.png"></div>
+          <div class="col">
+            <nav>
+              <router-link to="/">Home</router-link> |
+              <router-link to="/about">About</router-link> |
+              <router-link to="/random">Random</router-link>
+            </nav>
+          </div>
+          <div class="col">
+            <div>4 Privet Drive</div>
+            <div>Little Whinging, Surrey</div>
+            <div>England</div>
+          </div>
+        </div>
+  </footer>
 </template>
 
 <style>
@@ -26,5 +49,16 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+header {
+  padding: 10px 30px;
+  background-color: rgba(183, 224, 210, 20%)
+}
+.logoTop {
+  height: 100px;
+}
+.logoBottom {
+  height: 70px;
 }
 </style>
